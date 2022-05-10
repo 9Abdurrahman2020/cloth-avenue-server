87d2d49a920f404b048cce400da40d3c58948f6a
9abdurrahman2020 <9abdurrahman2020@gmail.com>
2022-05-10 15:02:09 +0600

    change port variable

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const ObjectId = require("mongodb").ObjectId;
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 const stripe = require("stripe")(process.env.stripe_secret);
